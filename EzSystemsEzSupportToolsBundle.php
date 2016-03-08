3875f305279a1ce20c48fc33637e248901f345b3
Gunnstein Lye <gunnstein.lye@ez.no>
2016-03-08 14:54:03 +0100

    Initial commit

---
+++ EzSystemsEzSupportToolsBundle.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * File containing the EzSystemsEzSupportToolsBundle class.
+ *
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+namespace EzSystems\EzSupportToolsBundle;
+
+use EzSystems\EzSupportToolsBundle\DependencyInjection\EzSystemsEzSupportToolsExtension;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class EzSystemsEzSupportToolsBundle extends Bundle
+{
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# eZ Support Tools
+
+This bundle provides information about the system eZ Platform/Studio is running on, intended to help system
+administrators and Support engineers.
