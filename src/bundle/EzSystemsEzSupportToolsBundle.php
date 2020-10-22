959d8f146589519fd0a9c33fd12fb2e45e43a7b5
Dawid Parafiński <dawid.parafinski@ez.no>
2020-10-22 14:43:45 +0200

    EZP-32067: Tagged ViewBuilder

--- src/bundle/EzSystemsEzSupportToolsBundle.php
+++ src/bundle/EzSystemsEzSupportToolsBundle.php
@@ -9,7 +9,6 @@ namespace EzSystems\EzSupportToolsBundle;
 use EzSystems\EzSupportToolsBundle\DependencyInjection\Compiler\SystemInfoCollectorPass;
 use EzSystems\EzSupportToolsBundle\DependencyInjection\Compiler\OutputFormatPass;
 use EzSystems\EzSupportToolsBundle\DependencyInjection\Compiler\SystemInfoTabGroupPass;
-use EzSystems\EzSupportToolsBundle\DependencyInjection\Compiler\ViewBuilderPass;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
@@ -20,7 +19,6 @@ class EzSystemsEzSupportToolsBundle extends Bundle
         parent::build($container);
         $container->addCompilerPass(new SystemInfoCollectorPass());
         $container->addCompilerPass(new OutputFormatPass());
-        $container->addCompilerPass(new ViewBuilderPass());
         $container->addCompilerPass(new SystemInfoTabGroupPass());
     }
 }
