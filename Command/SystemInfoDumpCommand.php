4a03aec08509fcb7998a993d30c09559749b11ad
André R <289757+andrerom@users.noreply.github.com>
2020-11-25 17:03:36 +0100

    [DX] Setup short forward compatible 'ibexa:info' aliases for system info dump command (#74)
    * Setup short alises for system info dump command
    
    * Update SystemInfoDumpCommand.php
    
    * Update SystemInfoDumpCommand.php

--- Command/SystemInfoDumpCommand.php
+++ Command/SystemInfoDumpCommand.php
@@ -47,6 +47,10 @@ class SystemInfoDumpCommand extends ContainerAwareCommand
     {
         $this
             ->setName('ez-support-tools:dump-info')
+            ->setAliases([
+                'ibexa:dump-info',
+                'ibexa:info',
+            ])
             ->setDescription('Collects system information and dumps it.')
             ->setHelp(<<<'EOD'
 By default it dumps information from all available information collectors.
