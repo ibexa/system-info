33a9109c56c503fa8a2e442a9d5f442bf2c01bc9
Dariusz Szut <dew326@gmail.com>
2022-01-17 14:08:07 +0100

    IBX-1920: Redesigned my ibexa in system information (#17)

---
+++ src/bundle/Resources/encore/ez.config.js
@@ -0,0 +1,7 @@
+const path = require('path');
+
+module.exports = (Encore) => {
+    Encore.addEntry('ibexa-system-information-css', [
+        path.resolve(__dirname, '../public/scss/system-information.scss'),
+    ]);
+};
