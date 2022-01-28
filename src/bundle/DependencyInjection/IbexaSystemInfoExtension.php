6657cf63d2395a44e4c7aa85637b53d5b220fe6e
Nattfarinn <nattfarinn@gmail.com>
2022-01-28 15:01:26 +0100

    [Rebranding] Expose raw edition identifier getter as a static method

--- src/bundle/DependencyInjection/IbexaSystemInfoExtension.php
+++ src/bundle/DependencyInjection/IbexaSystemInfoExtension.php
@@ -8,6 +8,7 @@ declare(strict_types=1);
 
 namespace Ibexa\Bundle\SystemInfo\DependencyInjection;
 
+use Composer\InstalledVersions;
 use Ibexa\Bundle\SystemInfo\SystemInfo\Collector\IbexaSystemInfoCollector;
 use Ibexa\Bundle\SystemInfo\SystemInfo\Value\IbexaSystemInfo;
 use Ibexa\Contracts\Core\Ibexa;
@@ -97,20 +98,23 @@ class IbexaSystemInfoExtension extends Extension implements PrependExtensionInte
             ],
         ]);
     }
-
-    public static function getNameByPackages(string $vendor): string
+    
+    public static function getEditionByPackages(): string
     {
-        if (is_dir($vendor . IbexaSystemInfoCollector::COMMERCE_PACKAGES[0])) {
-            $name = IbexaSystemInfo::PRODUCT_NAME_VARIANTS['commerce'];
-        } elseif (is_dir($vendor . IbexaSystemInfoCollector::EXPERIENCE_PACKAGES[0])) {
-            $name = IbexaSystemInfo::PRODUCT_NAME_VARIANTS['experience'];
-        } elseif (is_dir($vendor . IbexaSystemInfoCollector::CONTENT_PACKAGES[0])) {
-            $name = IbexaSystemInfo::PRODUCT_NAME_VARIANTS['content'];
-        } else {
-            $name = IbexaSystemInfo::PRODUCT_NAME_OSS;
+        if (InstalledVersions::isInstalled(IbexaSystemInfoCollector::COMMERCE_PACKAGES[0])) {
+            return 'commerce';
+        } else if (InstalledVersions::isInstalled(IbexaSystemInfoCollector::EXPERIENCE_PACKAGES[0])) {
+            return 'experience';
+        } else if (InstalledVersions::isInstalled(IbexaSystemInfoCollector::CONTENT_PACKAGES[0])) {
+            return 'content';
         }
+        
+        return 'oss';
+    }
 
-        return $name;
+    public static function getNameByPackages(string $vendor = null): string
+    {
+        return IbexaSystemInfo::PRODUCT_NAME_VARIANTS[self::getEditionByPackages()];
     }
 }
 
