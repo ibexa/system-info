241437ab1365a12ec74dfaf6bb7c6d71d74397b8
Dawid Parafiński <dawid.parafinski@ez.no>
2022-01-31 14:17:04 +0100

    IBX-2153: Moved ConfigResolverInterface into contracts

--- src/lib/Service/PersistenceCacheServiceInfo.php
+++ src/lib/Service/PersistenceCacheServiceInfo.php
@@ -8,7 +8,7 @@ declare(strict_types=1);
 
 namespace Ibexa\SystemInfo\Service;
 
-use Ibexa\Core\MVC\ConfigResolverInterface;
+use Ibexa\Contracts\Core\SiteAccess\ConfigResolverInterface;
 
 /**
  * @internal
